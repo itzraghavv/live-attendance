af62d408fcb0c90757051b3ef65ea12bbd364cd9
raghav-97 <raghavtandulkar97@gmail.com>
2026-02-02 19:39:20 +0530

    tweaks

--- index.ts
+++ index.ts
@@ -7,9 +7,10 @@ import type { JWTPayload } from "./lib/auth";
 import { signup } from "./routes/auth/signup";
 import { getStudents } from "./routes/class/get-students";
 import { startAttendance } from "./routes/class/start-attendance";
-import { getActiveSession } from "./lib/attendance-state";
+import { clearSession, getActiveSession } from "./lib/attendance-state";
 import { createClass } from "./routes/class/create-class";
 import { addStudent } from "./routes/class/add-student";
+import { AttendanceModel, ClassModel } from "./lib/db";
 
 await mongoose.connect(process.env.MONGO_DB_URL!);
 
@@ -21,8 +22,51 @@ mongoose.connection.on("error", (err) => {
   console.error(err);
 });
 
-const httpServer = Bun.serve({
+type WSData = JWTPayload;
+
+const server = Bun.serve({
   port: 3000,
+
+  fetch(req, server) {
+    const url = new URL(req.url);
+
+    if (url.pathname === "/ws") {
+      try {
+        const token = url.searchParams.get("token");
+
+        if (!token) {
+          return new Response("Unauthorized", { status: 401 });
+        }
+
+        const user = jwt.verify(token, process.env.JWT_SECRET!) as JWTPayload;
+        console.log(user);
+
+        if (!user) {
+          return new Response("invalid token", { status: 401 });
+        }
+
+        if (
+          server.upgrade(req, {
+            data: {
+              userId: user.userId,
+              role: user.role,
+            },
+          })
+        ) {
+          return;
+        }
+        return new Response("Upgrade failed", { status: 400 });
+      } catch (error: any) {
+        console.log(error);
+        return new Response(
+          JSON.stringify({
+            error: error.message,
+          }),
+        );
+      }
+    }
+  },
+
   routes: {
     "/": () => new Response("Hello"),
     "/auth/signup": signup,
@@ -33,100 +77,185 @@ const httpServer = Bun.serve({
     "/students": getStudents,
     "/attendance/start": startAttendance,
   },
-});
-
-console.log(`Server is running on ${httpServer.url}`);
-
-type WSData = JWTPayload;
-
-const wss = Bun.serve({
-  port: 8080,
-
-  fetch(req, server) {
-    const url = new URL(req.url);
-    console.log(url);
-    try {
-      const token = url.searchParams.get("token");
-
-      if (!token) {
-        return new Response("Unauthorized", { status: 401 });
-      }
-
-      const user = jwt.verify(token, process.env.JWT_SECRET!) as JWTPayload;
-      console.log(user);
-
-      if (!user) {
-        return new Response("invalid token", { status: 401 });
-      }
-
-      if (
-        server.upgrade(req, {
-          data: {
-            userId: user.userId,
-            role: user.role,
-          },
-        })
-      ) {
-        return;
-      }
-      return new Response("Upgrade failed", { status: 400 });
-    } catch (error: any) {
-      console.log(error);
-      return new Response(
-        JSON.stringify({
-          error: error.message,
-        }),
-      );
-    }
-  },
 
   websocket: {
     data: {} as WSData,
-    open(ws) {
-      ws.subscribe("class-room");
-      console.log("subscribed", ws.remoteAddress);
-      console.log("WS connected");
-    },
 
-    message(ws, message) {
+    open: (ws) => {
+      ws.subscribe("class-room");
+      console.log("connected", ws.remoteAddress);
+    },
+    message: (ws, message) => {
       handleMessage(ws, message);
     },
-
-    // close(ws) {
-    //   handleDisconnect(ws);
-    // },
   },
 });
 
-function handleMessage(ws, rawMessage) {
+console.log(`Server is running on ${server.url}`);
+
+async function handleMessage(ws, rawMessage) {
   const message = JSON.parse(rawMessage);
   console.log(message);
   console.log(ws.data);
+  let activeSession = getActiveSession();
+
+  if (!activeSession) {
+    ws.send(
+      JSON.stringify({
+        event: "Error",
+        data: {
+          message: "no session",
+        },
+      }),
+    );
+    return;
+  }
+
   if (message.event === "ATTENDANCE_MARKED") {
     if (ws.data.role != "teacher") {
       ws.send(
         JSON.stringify({
-          error: "something went wrong",
-          message: "unauthorized",
-        }),
-      );
-      return;
-    }
-
-    let activeSession = getActiveSession();
-
-    if (!activeSession) {
-      ws.send(
-        JSON.stringify({
-          error: "invalid session",
-          message: "no session",
+          event: "Error",
+          data: {
+            message: "unauthorized",
+          },
         }),
       );
       return;
     }
 
     activeSession.attendance[message.data.studentId] = message.data.status;
-    console.log("Publishing to class-room:", message);
-    wss.publish("class-room", JSON.stringify(message));
+    server.publish("class-room", JSON.stringify(message));
+  }
+
+  if (message.event === "TODAY_SUMMARY") {
+    if (ws.data.role != "teacher") {
+      ws.send(
+        JSON.stringify({
+          event: "Error",
+          data: {
+            message: "teacher role required",
+          },
+        }),
+      );
+      return;
+    }
+
+    const values = Object.values(activeSession.attendance);
+
+    const stats = {
+      present: values.filter((v) => v === "present").length,
+      absent: values.filter((v) => v === "absent").length,
+      total: values.length,
+    };
+
+    server.publish(
+      "class-room",
+      JSON.stringify({
+        event: "TODAY_SUMMARY",
+        data: stats,
+      }),
+    );
+  }
+
+  if (message.event === "MY_ATTENDANCE") {
+    if (ws.data.role != "student") {
+      ws.send(
+        JSON.stringify({
+          event: "Error",
+          data: {
+            message: "student role required",
+          },
+        }),
+      );
+      return;
+    }
+
+    const status =
+      activeSession.attendance[ws.data.userId] ?? "not updated yet";
+
+    ws.send(
+      JSON.stringify({
+        event: "MY_ATTENDANCE",
+        data: {
+          status,
+        },
+      }),
+    );
+  }
+
+  if (message.event === "DONE") {
+    if (ws.data.role != "teacher") {
+      ws.send(
+        JSON.stringify({
+          event: "Error",
+          data: {
+            message: "teacher role required",
+          },
+        }),
+      );
+      return;
+    }
+
+    const { classId, attendance, startedAt } = activeSession;
+
+    const studentsInClass =
+      await ClassModel.findById(classId).select("studentIds");
+
+    if (!studentsInClass) {
+      ws.send(
+        JSON.stringify({
+          event: "Error",
+          data: {
+            message: "Class not found",
+          },
+        }),
+      );
+      return;
+    }
+
+    const students = studentsInClass?.studentIds.map(String);
+
+    for (const studentId of students) {
+      if (!attendance[studentId]) {
+        attendance[studentId] = "absent";
+      }
+    }
+
+    const attendanceDocs = Object.entries(activeSession.attendance).map(
+      ([studentId, status]) => ({
+        classId: activeSession.classId,
+        studentId,
+        status,
+      }),
+    );
+
+    await AttendanceModel.insertMany(attendanceDocs);
+
+    // await AttendanceModel.create({
+    //   classId,
+    //   studentId: students,
+    // });
+
+    const values = Object.values(attendance);
+
+    const summary = {
+      present: values.filter((v) => v === "present").length,
+      absent: values.filter((v) => v === "absent").length,
+      total: values.length,
+    };
+
+    clearSession();
+
+    server.publish(
+      "class-room",
+      JSON.stringify({
+        event: "DONE",
+        data: {
+          message: "Attendance persisted",
+          ...summary,
+        },
+      }),
+    );
   }
 }
